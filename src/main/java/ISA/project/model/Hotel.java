8be1fc68e4b2854976bbe5b62a15b03b45339d4f
NikolaPavlovic1 <44706282+NikolaPavlovic1@users.noreply.github.com>
2018-12-30 10:42:09 +0100

    Add files via upload
    izmena modela, dodate klase

---
+++ src/main/java/ISA/project/model/Hotel.java
@@ -0,0 +1,87 @@
+package ISA.project.model;
+
+import java.util.ArrayList;
+
+public class Hotel {
+
+	private String ime;
+	private String adresa;
+	private String opis;
+	private ArrayList<Integer> ocene;
+	private double prihod;
+	private ArrayList<Soba> sobe;
+	private int brojSlobodnihSoba;
+	private ArrayList<Soba> cenovnik;
+
+	public Hotel(String ime, String adresa, String opis, ArrayList<Soba> sobe) {
+		super();
+		this.ime = ime;
+		this.adresa = adresa;
+		this.opis = opis;
+		this.sobe = sobe;
+	}
+
+	public String getIme() {
+		return ime;
+	}
+
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+
+	public String getAdresa() {
+		return adresa;
+	}
+
+	public void setAdresa(String adresa) {
+		this.adresa = adresa;
+	}
+
+	public String getOpis() {
+		return opis;
+	}
+
+	public void setOpis(String opis) {
+		this.opis = opis;
+	}
+
+	public ArrayList<Integer> getOcene() {
+		return ocene;
+	}
+
+	public void setOcene(ArrayList<Integer> ocene) {
+		this.ocene = ocene;
+	}
+
+	public double getPrihod() {
+		return prihod;
+	}
+
+	public void setPrihod(double prihod) {
+		this.prihod = prihod;
+	}
+
+	public ArrayList<Soba> getSobe() {
+		return sobe;
+	}
+
+	public void setSobe(ArrayList<Soba> sobe) {
+		this.sobe = sobe;
+	}
+	
+	public int getBrojSlobodnihSoba() {
+		return brojSlobodnihSoba;
+	}
+
+	public void setBrojSlobodnihSoba(int brojSlobodnihSoba) {
+		this.brojSlobodnihSoba = brojSlobodnihSoba;
+	}
+
+	public ArrayList<Soba> getCenovnik() {
+		return cenovnik;
+	}
+
+	public void setCenovnik(ArrayList<Soba> cenovnik) {
+		this.cenovnik = cenovnik;
+	}
+}
