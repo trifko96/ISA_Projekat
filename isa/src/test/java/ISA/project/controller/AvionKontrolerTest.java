7547298cce4839fe4c0cf041f8ee35da5938a214
trifko96 <trifko96@gmail.com>
2019-09-08 13:00:50 +0200

    JUnit testovi

---
+++ isa/src/test/java/ISA/project/controller/AvionKontrolerTest.java
@@ -0,0 +1,82 @@
+package ISA.project.controller;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.nio.charset.Charset;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import ISA.project.dto.AvionDTO;
+import ISA.project.utils.TestUtil;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AvionKontrolerTest {
+
+	private static final String URL_PREFIX = "/Avion";
+	
+	private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+			MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+	
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void testVratiAvione() throws Exception {
+		mockMvc.perform(get(URL_PREFIX + "/vratiAvione/7" )).andExpect(status().isOk())
+		.andExpect(content().contentType(contentType))
+		.andExpect(jsonPath("$.[*].id").value(hasItem(1)))
+		.andExpect(jsonPath("$.[*].id").value(hasItem(2)));
+	}
+	
+	@Test
+	public void testVratiAvioneZaLet() throws Exception {
+		mockMvc.perform(get(URL_PREFIX + "/vratiAvioneZaLet/7" )).andExpect(status().isOk())
+		.andExpect(content().contentType(contentType))
+		.andExpect(jsonPath("$.[*].id").value(hasItem(2)));
+	}
+	
+	@Test
+	public void testVratiAvion() throws Exception {
+		mockMvc.perform(get(URL_PREFIX + "/vratiAvion/1" )).andExpect(status().isOk())
+		.andExpect(content().contentType(contentType))
+		.andExpect(jsonPath("$.id").value(1));
+	}
+	
+}
