90b8ccb9e73c3a8b55085104545797dcaecb1a89
trifko96 <trifko96@gmail.com>
2019-09-08 17:12:46 +0200

    testovi

--- isa/src/test/java/ISA/project/controller/AerodromKontrolerTest.java
+++ isa/src/test/java/ISA/project/controller/AerodromKontrolerTest.java
@@ -9,6 +9,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -27,6 +29,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 import ISA.project.dto.AerodromDTO;
+import ISA.project.dto.AvionskaKartaDTO;
 import ISA.project.dto.LetDTO;
 import ISA.project.utils.TestUtil;
 
@@ -83,6 +86,23 @@ private static final String URL_PREFIX = "/Aerodrom";
 		mockMvc.perform(get(URL_PREFIX + "/vratiSlobodneAerodrome/7" )).andExpect(status().isOk())
 		.andExpect(content().contentType(contentType))
 		.andExpect(jsonPath("$.[*].id").value(3));
+	}
+	
+	@Transactional
+	@Rollback(true)
+	@Test
+	public void testDodajPostojeci() throws Exception {
+		AerodromDTO a = new AerodromDTO();
+		a.setGrad("Mostar");
+		a.setId(3);
+		a.setIme("Medjunarodni aerodrom");
+		List<AerodromDTO> lista = new ArrayList<>();
+		lista.add(a);
+		String json = ISA.project.utils.TestUtil.json(lista);
+		this.mockMvc.perform(post(URL_PREFIX + "/dodajPostojeci/7").contentType(contentType).content(json)).andExpect(status().isOk())
+		.andExpect(jsonPath("$.[*].id").value(hasItem(1)))
+		.andExpect(jsonPath("$.[*].id").value(hasItem(2)))
+		.andExpect(jsonPath("$.[*].id").value(hasItem(3)));
 	}
 	
 	
